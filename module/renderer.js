52e7a0e87eec678bf737745450dffeb4036f8d74
Dragon-Fish <824399619@qq.com>
2021-01-07 22:23:23 +0800

    Update theme & renderer

--- module/renderer.js
+++ module/renderer.js
@@ -1,6 +1,6 @@
 const { fromApi } = require('./parser')
 const actionGet = require('./actionGet')
-const { util } = require('./mw')
+const { util, hook } = require('./mw')
 const log = require('./log')
 
 // 获取帖子元素
@@ -54,46 +54,17 @@ function renderAllForums({ forumEl, theme }) {
 function renderForum(ctx, $root) {
   var { _forum, forumEl, forumid, theme } = ctx
   var threads = forumEl.threads
-  $root = $root || $(theme.forumContainer({ meta: forumEl.meta }))
-
-  var newThreadArea = getNewThreadArea({
-    forumEl: _forum,
-    forumid: _forum.meta.id,
-  })
+  $root = $root || theme.forumContainer({ meta: forumEl.meta })
 
   $.each(threads, (index, item) => {
     // 缓存帖子对象
-    var $thread
-
-    if (index === 0 && forumEl.title) {
-      // 楼主
-      $thread = theme.firstThread({
-        meta: item.meta,
-        content: item.content,
-        fn: {
-          newThreadArea,
-          newReplyArea: getNewReplyArea({
-            forumEl: _forum,
-            forumid,
-            threadid: item.id,
-          }),
-        },
-      })
-    } else {
-      // 一般楼层
-      $thread = theme.normalThread({
-        meta: item.meta,
-        content: item.content,
-        fn: {
-          newThreadArea,
-          newReplyArea: getNewReplyArea({
-            forumEl: _forum,
-            forumid,
-            threadid: item.id,
-          }),
-        },
-      })
-    }
+    var $thread = theme.threadContainer({
+      _forum,
+      forumid,
+      meta: item.meta,
+      content: item.content,
+      fn,
+    })
 
     // 如果有回复，处理回复
     if (item.threads && item.threads.length > 0) {
@@ -108,37 +79,7 @@ function renderForum(ctx, $root) {
   return $root
 }
 
-// 生成回复框
-function getNewReplyArea({ forumEl, forumid, threadid }) {
-  var $replyBtn = $('<a>', { text: '回复', href: 'javascript:;' })
-  var $modifyBtn = $('<a>', { text: '编辑', href: 'javascript:;' })
-  var $deleteBtn = $('<a>', { text: '删除', href: 'javascript:;' })
-  var $textArea = $('<textarea>', { class: 'forum-textarea' })
-  var $submitBtn = $('<button>', {
-    text: '提交',
-    class: 'forum-submit-btn',
-  })
-
-  var $container = $('<div>', {
-    class: 'forum-new-reply-area',
-    'data-debug': JSON.stringify({
-      forumid,
-      threadid,
-    }),
-  }).append(
-    $('<div>', { class: 'forum-modify-container' }).append(
-      $replyBtn,
-      $modifyBtn,
-      $deleteBtn
-    ),
-    $('<label>', { class: 'forum-input-container' }).append(
-      $('<div>').append($textArea),
-      $('<div>').append($submitBtn)
-    )
-  )
-
-  return $container
-}
+function renderThread() {}
 
 // 生成新楼框
 function getNewThreadArea({ forumEl, forumid }) {
@@ -169,6 +110,11 @@ function getNewThreadArea({ forumEl, forumid }) {
   )
 
   return $container
+}
+
+var fn = {
+  parser: require('./parser'),
+  updater: require('./updater'),
 }
 
 // 从页面加载内容，并渲染到根元素
@@ -205,8 +151,9 @@ function toPage(forumEl, target = '#mw-content-text') {
    * @param {Object} ctx 传入的上下文
    * @param {Functon} next 返回的主题渲染器
    */
-  mw.hook('WikiForum.theme').fire(theme => {
+  hook('WikiForum.theme').fire(theme => {
     $(target).html(renderAllForums({ forumEl, theme }))
+    hook('WikiForum.renderer').fire()
   })
 }
 
--- theme/default/default.js
+++ theme/default/default.js
@@ -1,76 +1,107 @@
 /**
  * @function theme.default 标准的官方主题
  * @param {Object} ctx
- * @param {jQuery<Element>} ctx.fn.newThreadArea
- * @param {jQuery<Element>} ctx.fn.newReplyArea
- * @param {Object} ctx.meta
+ * @param {Object} ctx.meta Metadata
  *
  * @param {Function} next
  */
 mw.hook('WikiForum.theme').add(next => {
+  // 全论坛容器
   var allForumsContainer = ctx => {
-    return $('<div>', { class: 'wiki-forum-all-container' })
-  }
-
-  var forumContainer = ctx => {
-    return $('<div>', { class: 'wiki-forum', 'data-forumid': ctx.meta.id })
-  }
-
-  var firstThread = ctx => {
-    return $('<div>', { class: 'forum-thread forum-first' }).append(
-      $('<div>', { class: 'forum-before' }).append(
-        $('<h3>', { class: 'forum-title', text: ctx.meta.title }),
-        $('<span>', { class: 'forum-id-link', text: '#' + ctx.meta.id }),
-        $('<div>', { class: 'forum-user' }).append(
-          $('<span>', { class: 'forum-user-link' }).append(
-            $('<a>', {
-              text: ctx.meta.userAuthor,
-              href: mw.util.getUrl('User:' + ctx.meta.userAuthor),
-            })
-          )
-        )
-      ),
-      $('<div>', { class: 'forum-content', html: ctx.content }),
-      $('<div>', { class: 'forum-after' }).append(
-        $('<i>', {
-          class: 'post-date',
-          text: new Date(ctx.meta.timePublish).toLocaleString(),
-        })
-      ),
-      ctx.fn.newThreadArea
+    return $('<div>', { class: 'wiki-forum-all-container' }).append(
+      newForumContainer()
     )
   }
 
-  var normalThread = ctx => {
-    return $('<div>', { class: 'forum-thread' }).append(
-      $('<div>', { class: 'forum-before' }).append(
-        $('<div>', { class: 'forum-user' }).append(
-          $('<span>', { class: 'forum-user-link' }).append(
-            $('<a>', {
-              text: ctx.meta.userAuthor,
-              href: mw.util.getUrl('User:' + ctx.meta.userAuthor),
-            })
-          )
-        )
-      ),
-      $('<div>', { class: 'forum-content', html: ctx.content }),
-      $('<div>', { class: 'forum-after' }).append(
-        $('<i>', {
-          class: 'post-date',
-          text: new Date(ctx.meta.timePublish).toLocaleString(),
-        }),
-        ctx.fn.newReplyArea
+  // 单论坛容器
+  var forumContainer = ctx => {
+    return $('<div>', {
+      class: 'wiki-forum',
+      'data-forumid': ctx.meta.id,
+    }).append(newThreadContainer())
+  }
+
+  // 帖子容器
+  var threadContainer = ctx => {
+    // 处理 meta
+    const id = String(ctx.meta.id)
+    const content = cxt.content
+    const timePublish =
+      ctx.meta.timePublish || ctx.meta.timeRelease || ctx.meta.release || ''
+    const timeModify = ctx.meta.timeModify || timePublish
+    const userAuthor = ctx.meta.userAuthor || ctx.meta.user || 'unsigned'
+    const userLast = ctx.meta.userLast || userAuthor
+
+    // 缓存组件
+    var $idLink = $('<span>', { class: 'forum-id-link', text: '#' + id })
+    var $userLink = $('<div>', { class: 'forum-user' }).append(
+      $('<span>', { class: 'forum-user-link' }).append(
+        $('<a>', {
+          class: 'mw-userlink userAuthor',
+          text: userAuthor,
+          href: mw.util.getUrl('User:' + userAuthor),
+        })
       )
     )
+    var $content = $('<div>', { class: 'forum-content', html: content })
+    var $timeArea = $('<div>', { class: 'post-time' }).append(
+      $('<i>', {
+        class: 'post-date timePublish',
+        text: new Date(timePublish).toLocaleString(),
+      })
+    )
+
+    // 楼主
+    var $firstThread = $('<div>', { class: 'forum-thread forum-first' }).append(
+      $('<div>', { class: 'forum-before' }).append(
+        $('<h3>', { class: 'forum-title', text: ctx.meta.title }),
+        $idLink,
+        $userLink
+      ),
+      $content,
+      $('<div>', { class: 'forum-after' }).append($timeArea)
+    )
+
+    // 普通帖子
+    var $normalThread = $('<div>', { class: 'forum-thread' }).append(
+      $('<div>', { class: 'forum-before' }).append($idLink, $userLink),
+      $content,
+      $('<div>', { class: 'forum-after' }).append(
+        $timeArea,
+        newReplyContainer()
+      )
+    )
+
+    // 判断是否为楼主，并返回帖子容器
+    if (id === '1') {
+      return $firstThread
+    } else {
+      return $normalThread
+    }
   }
+
+  // 新回复容器
+  var newReplyContainer = ctx => {
+    return $('<div>').append($('<p>', { text: 'newReplyContainer' }))
+  }
+
+  // 新帖子容器
+  var newThreadContainer = ctx => {
+    return $('<div>').append($('<p>', { text: 'newThreadContainer' }))
+  }
+
+  // 新论坛容器
+  var newForumContainer = ctx => {
+    return $('<div>').append($('<p>', { text: 'newForumContainer' }))
+  }
+
+  // 无论坛容器
+  var noForumContainer = ctx => {}
 
   next &&
     next({
       allForumsContainer,
       forumContainer,
-      // beforeForum,
-      firstThread,
-      normalThread,
-      // afterForum,
+      threadContainer,
     })
 })
