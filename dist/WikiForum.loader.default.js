3350bf60bb4736c1dedbe9b8207e94c52f1a8c9b
Dragon-Fish <Dragon-Fish@users.noreply.github.com>
2021-01-12 05:59:50 +0000

    deploy: 30a1c9ec52bf1d4562b60ef685d495bfe1974810

--- dist/WikiForum.loader.default.js
+++ dist/WikiForum.loader.default.js
@@ -13,9 +13,22 @@
   !*** ./loader/default.js ***!
   \***************************/
 mw.hook('WikiForum').add(function (Core) {
-  var loadNS = window.WikiForumNS || [];
-  if (typeof loadNS === 'number') loadNS = [loadNS];
   var conf = mw.config.get();
+  var settings = window.WikiForumLoaderDefault || {};
+  var loadNS = settings.loadNS || window.WikiForumNS || [];
+
+  if (typeof loadNS === 'string') {
+    loadNS = Number(loadNS);
+
+    if (isNaN(loadNS)) {
+      loadNS = [];
+    } else {
+      loadNS = [loadNS];
+    }
+  }
+
+  if (typeof loadNS === 'number') loadNS = [loadNS];
+  if (loadNS.length < 1 && conf.wgNamespaceIds.forum) loadNS = [conf.wgNamespaceIds.forum];
 
   if (loadNS.includes(conf.wgNamespaceNumber) && $('.wiki-forum').length > 0 && conf.wgArticleId !== 0) {
     Core.renderer.fromPage(conf.wgPageName);
